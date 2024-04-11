5f1761f3fd2c860384fc00ddc9524e8b2bbcf6cb
Yousef Zedan <165020329+Yousef-zedan@users.noreply.github.com>
2024-04-11 18:07:55 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Scroll To Top
+// let span = document.querySelector(".up");
+
+// window.onscroll = function () {
+//     if ( this.scrollY >= 600) {
+//         span.classList.add("show");
+//     } else {
+//         span.classList.remove("show");
+//     }
+// }
+
+// span.onclick = function () {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     });
+// };
+
+
